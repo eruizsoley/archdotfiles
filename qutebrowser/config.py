c19d500cc68aa3f047576e7fb45e6918e0357513
naggurn <eruizsoley@gmail.com>
2021-12-28 19:45:36 +0100

    dvorak setup

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,3 +1,9 @@
 config.load_autoconfig(False)
 config.set("colors.webpage.darkmode.enabled", True)
 config.source("nord-qutebrowser.py")
+
+config.bind('t', 'scroll down')
+config.bind('n', 'scroll up')
+config.bind('s', 'scroll left')
+config.bind('u', 'hint')
+
